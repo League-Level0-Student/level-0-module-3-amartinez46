df70c929dc4994b4accf747c9e6b5f56a835c0e7
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-02-20 19:23:46 -0800

    birthday

--- src/elseif/BirthdayReminder.java
+++ src/elseif/BirthdayReminder.java
@@ -6,19 +6,34 @@ package elseif;
 
 import javax.swing.JOptionPane;
 
+import org.omg.Messaging.SyncScopeHelper;
+
 public class BirthdayReminder {
 
 	public static void main(String[] args) {
 	
 		// 1. correct the birthdays for your family below
-		String momsBirthday = "January 24th";
-		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
+		String momsBirthday = "November 30";
+		String dadsBirthday = "Septmber 8";
+		String myBirthday = "May 26";
 
 		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+	String birthday= JOptionPane.showInputDialog(null, "Which birthday do you want to use?");
 		// 3. Print out what the user typed
+	if (birthday.equals("mom")) {
 		
+	System.out.println(birthday);
+		System.out.println("Mom's birthday is on November 30");}
+		if (birthday.equals("dad")){
+			
+		System.out.println("Dad's birthday is on September 8");
+		}
+		if (birthday.equals("me")) {
+			System.out.println("My birthday is on May 26");
+		}
+		else if (birthday.equals("sister")) {
+			JOptionPane.showConfirmDialog( "Sorry I don't know that person's birthday!");
+		}
 		// 4. if user asked for "mom"
 			//print mom's birthday
 		// 5. if user asked for "dad"
@@ -27,5 +42,10 @@ public class BirthdayReminder {
 			// print myBirthday
 		//7. otherwise print "Sorry, i don't remember that person's birthday!"
 
+	}
+
+	private static void SyncScopeHelper(String momsBirthday) {
+		// TODO Auto-generated method stub
+		
 	} 
 }
