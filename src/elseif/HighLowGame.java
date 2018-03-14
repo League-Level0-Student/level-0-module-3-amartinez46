d12f76f07e7b5269808213a05fedc97d4c78f301
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-03-13 19:20:51 -0700

    high robot

--- src/elseif/HighLowGame.java
+++ src/elseif/HighLowGame.java
@@ -10,41 +10,53 @@ import javax.swing.JOptionPane;
 public class HighLowGame {
 
 	public static void main(String[] args) {
-		// 3. Change this line to give you a random number between 1 - 100. 
+		// 3. Change this line to give you a random number between 1 - 100.
 		int random = new Random().nextInt(100);
-		
-		
+
 		// 2. Print out the random variable above
 		System.out.println(random);
 		// 11. Repeat steps 1 to 10 ten times
-		
-			// 1. Ask the user for a guess using a pop-up window, and save their response 
-String guess=JOptionPane.showInputDialog(null, "Give me a guess");
-			
+		for (int i = 0; i < 10; i++) {
 
+			// 1. Ask the user for a guess using a pop-up window, and save their response
+			String guess = JOptionPane.showInputDialog(null, "Give me a guess");
 
 			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
-int answer=Integer.parseInt(guess);
+			int answer = Integer.parseInt(guess);
 			// 5. if the guess is correct
-			if (answer==random) {
-				JOptionPane.showMessageDialog(null,"You win!");
-			}
+			if (answer == random) {
+				JOptionPane.showMessageDialog(null, "You win!");
+				System.exit(0);
+			} else {
+				if (answer > random) {
+					JOptionPane.showMessageDialog(null, "Too high!");
 
-// 6. Win
-				// 12. Use "System.exit(0);" to quit the game if the user guessed the right answer.
-			// 7. if the guess is high
-				if (answer<random) {
-					JOptionPane.showMessageDialog(null,"Too high!");
 				}
+				if (answer < random) {
+					JOptionPane.showMessageDialog(null, "Too low!");
+				}
+			}
+			if (i==9) {
+				
 			
-			// 8. Tell them it's too high
-			// 9. if the guess is low
-				// 10. Tell them it's too low
+			JOptionPane.showMessageDialog(null, "You lose!");
+		}
+		}
 
-		// 13. Tell them they lose
-		
+		// 6. Win
+		// 12. Use "System.exit(0);" to quit the game if the user guessed the right
+		// answer.
+
+		// 7. if the guess is high
+
+		// 8. Tell them it's too high
+
 	}
 
+	// 9. if the guess is low
+
+	// 10. Tell them it's too low
+
+	// 13. Tell them they lose
+
 }
-
-
