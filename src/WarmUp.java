524e96ed27587f55e05f475ae665a7f538a1d617
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-03-27 19:21:13 -0700

    warm high color

---
+++ src/WarmUp.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+public class WarmUp {
+public static void main(String[] args) {
+	String ans=JOptionPane.showInputDialog(null,"Give me a number.");
+int answer= Integer.parseInt(ans);
+	String answers=JOptionPane.showInputDialog(null,"Give me a word.");
+	for (int i = 0; i < answer; i++) {
+	System.out.println(answers);
+}
+
+
+
+
+
+}
+}
--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -4,6 +4,7 @@
 package elseif;
 
 import javax.swing.JOptionPane;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MoveAction;
 
 import org.jointheleague.graphical.robot.Robot;
 
@@ -12,20 +13,37 @@ import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
+		
 		//1. Create a new Robot
 		Robot r2d2= new Robot();
 		//3. Ask the user what color they would like the robot to draw
-		
+String pen= JOptionPane.showInputDialog(null, "What color would you like?");
 		//5. Use an if/else statement to set the pen color that the user requested
-
-        //6. If the user doesn’t enter anything, choose a random color
-
+if (pen.equals("blue")) {
+	r2d2.setPenColor(Color.blue);
+}
+if (pen.equals("pink")) {
+	r2d2.setPenColor(Color.pink);
+}
+if (pen.equals("black")) {
+	r2d2.setPenColor(Color.black);
+}        //6. If the user doesn’t enter anything, choose a random color
+if (pen.equals("blue")) {
+	r2d2.setPenColor(Color.blue);
+}
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
-		
+	
 		//4. Set the pen width to 10
-		
+		r2d2.setPenWidth(10); 
 	    //2. Make the robot draw a shape (this will take more than one line of code)
-for(int i=0,i<6,i++) {
-r2d2.
+r2d2.penDown();
+	r2d2.move(110);
+	r2d2.turn(90);
+	r2d2.move(110);
+	r2d2.turn(90);
+	r2d2.move(110);
+	r2d2.turn(90);
+	r2d2.move(110);
+	
 	}
 }
